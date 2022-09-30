23b09ab419ae177a7706738337e2c253e63e1f61
spoookyghost <52107169+spoookyghost@users.noreply.github.com>
2022-09-30 19:53:59 +0200

    Update integrationResearch.sql

--- integrationResearch.sql
+++ integrationResearch.sql
@@ -1,7 +1,3 @@
--- Student Name: Ambroise Reynier
--- Student Number : 20036699
--- Write your commands and/or comments below
-
 -- Here are my two research questions:
 -- 1) Is there a correlation between the number of measures and the case fatality ratio?
 -- 2) Is there a correlation between the lenght of the lockdown (enforced by most countries in the world) and the case fatality ratio?
