46f47e614a8822056f64ad1a66f9fc467b6e89e2
spoookyghost <52107169+spoookyghost@users.noreply.github.com>
2022-09-30 19:53:27 +0200

    Update corona2DM.sql

--- corona2DM.sql
+++ corona2DM.sql
@@ -1,7 +1,3 @@
--- Student Name: Ambroise Reynier
--- Student Number : 20036699
--- Write your commands and/or comments below
-
 -- Star schema
 
 CREATE TABLE IF NOT EXISTS country_dimension AS (SELECT DISTINCT (countryterritoryCode),
