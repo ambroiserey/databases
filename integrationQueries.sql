be0edab61662bbb0111b85dce3ed88a2d77b4b02
spoookyghost <52107169+spoookyghost@users.noreply.github.com>
2022-09-30 19:53:53 +0200

    Update integrationQueries.sql

--- integrationQueries.sql
+++ integrationQueries.sql
@@ -1,7 +1,3 @@
--- Student Name: Ambroise Reynier
--- Student Number : 20036699
--- Write your commands and/or comments below
-
 -- 1) Is there a correlation between the number of measures and the case fatality ratio?
 
 SELECT 
