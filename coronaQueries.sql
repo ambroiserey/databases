1dec0e8445d8d0a577b3ac694de6acc5a81ebba1
spoookyghost <52107169+spoookyghost@users.noreply.github.com>
2022-09-30 19:53:33 +0200

    Update coronaQueries.sql

--- coronaQueries.sql
+++ coronaQueries.sql
@@ -1,7 +1,3 @@
--- Student Name: Ambroise Reynier
--- Student Number : 20036699
--- Write your commands and/or comments below
-
 -- 1) What is the global case fatality ratio of the coronavirus?
 
 SELECT 
