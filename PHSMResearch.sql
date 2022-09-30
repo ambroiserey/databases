7925e840e558c71173a763084e00a75d0b77a0bf
spoookyghost <52107169+spoookyghost@users.noreply.github.com>
2022-09-30 19:53:13 +0200

    Update PHSMResearch.sql

--- PHSMResearch.sql
+++ PHSMResearch.sql
@@ -1,7 +1,3 @@
--- Student Name: Ambroise Reynier
--- Student Number : 20036699
--- Write your commands and/or comments below
-
 -- Here are my two research questions:
 -- 1) What were the most common measures taken?
 -- 2) What was the lenght of the first French lockdown?
